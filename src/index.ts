7f179c74e93f32766d3e60975cab1d055d6aff42
Yavisth <yavisth.chopra@gmail.com>
2024-02-08 15:27:01 +0530

    final

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import compression from "compression";
 import mongoose from "mongoose";
+import router from "./router";
 
 const app = express();
 
@@ -31,3 +32,4 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
 
+app.use('/', router());
